90d37390be953990794923c86b66c688acf37bea
Caio Santos <caio.santos@joinplank.com>
2025-07-31 11:50:46 -0300

    fix middleware

--- apps/ai-chat-web/lib/supabase/middleware.ts
+++ apps/ai-chat-web/lib/supabase/middleware.ts
@@ -53,7 +53,7 @@ export async function updateSession(request: NextRequest) {
 			request.nextUrl.pathname === "/")
 	) {
 		const url = request.nextUrl.clone();
-		url.pathname = "/";
+		url.pathname = "/home";
 		return NextResponse.redirect(url);
 	}
 
