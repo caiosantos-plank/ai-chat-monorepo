cf8988518be6ca573f65024f37ab100633b305be
Caio Santos <caio.santos@joinplank.com>
2025-08-06 15:56:47 -0300

    Fixing tools

--- apps/ai-chat-web/lib/langgraph/supervisor.multiagent.ts
+++ apps/ai-chat-web/lib/langgraph/supervisor.multiagent.ts
@@ -15,7 +15,12 @@ import {
 	RemoveMessage,
 } from "@langchain/core/messages";
 import z from "zod";
-import { tools } from "./tools";
+import {
+	searchToolNode,
+	searchTools,
+	weatherToolNode,
+	weatherTools,
+} from "./tools";
 
 const AgentState = Annotation.Root({
 	...MessagesAnnotation.spec,
@@ -39,8 +44,8 @@ export class Supervisor {
 		this.model = model;
 	}
 
-	private hasToContinue(state: typeof MessagesAnnotation.State) {
-		const { messages } = state;
+	private hasToContinue(state: typeof AgentState.State) {
+		const { messages, goingTo } = state;
 		const lastMessage = messages.at(-1);
 
 		if (
@@ -49,28 +54,13 @@ export class Supervisor {
 			Array.isArray(lastMessage.tool_calls) &&
 			lastMessage.tool_calls?.length
 		) {
-			return "tools";
+			return goingTo;
 		}
 		return END;
 	}
 
 	private weatherAgent = async (state: typeof AgentState.State) => {
-		if (
-			state.agentCalls?.weather_expert &&
-			state.agentCalls.weather_expert > 2
-		) {
-			return new Command({
-				goto: "chat_agent",
-				update: {
-					messages: [
-						...state.messages,
-						new AIMessage("I already gave the weather information"),
-					],
-					goingTo: "chat_agent",
-				},
-			});
-		}
-		const modelWithTools = this.model.bindTools(tools.tools);
+		const modelWithTools = this.model.bindTools(weatherTools);
 
 		const response = await modelWithTools.invoke([
 			{
@@ -79,23 +69,21 @@ export class Supervisor {
 					"You are a weather expert, and you are responsible to provide the weather information.",
 					" You cannot answer questions that are not related to the weather.",
 					" You have access to the following tools: get_weather.",
-					"You must send just the location to the get_weather tool, do not include any other text.",
-					"Do not include words like city, country, state, etc. Just the location name.",
+					" You must send just the location to the get_weather tool, do not include any other text.",
+					" Do not include words like city, country, state, etc. Just the location name.",
 					" You must avoid call to the same tool twice.",
 				].join(""),
 			},
 			...state.messages,
 		]);
 
-		console.log("weather agent called");
-
 		if (response.tool_calls && response.tool_calls.length > 0) {
 			return new Command({
-				goto: "tools",
+				goto: "weather_tool",
 				update: {
 					...state.agentCalls,
 					messages: response,
-					goingTo: "tools",
+					goingTo: "weather_tool",
 				},
 			});
 		}
@@ -114,19 +102,7 @@ export class Supervisor {
 	};
 
 	private newsAgent = async (state: typeof AgentState.State) => {
-		if (state.agentCalls?.news_expert && state.agentCalls.news_expert > 2) {
-			return new Command({
-				goto: "chat_agent",
-				update: {
-					messages: [
-						...state.messages,
-						new AIMessage("I already gave the news information"),
-					],
-					goingTo: "chat_agent",
-				},
-			});
-		}
-		const modelWithTools = this.model.bindTools(tools.tools);
+		const modelWithTools = this.model.bindTools(searchTools);
 
 		const response = await modelWithTools.invoke([
 			{
@@ -142,15 +118,13 @@ export class Supervisor {
 			...state.messages,
 		]);
 
-		console.log("news agent called");
-
 		if (response.tool_calls && response.tool_calls.length > 0) {
 			return new Command({
-				goto: "tools",
+				goto: "search_tool",
 				update: {
 					...state.agentCalls,
 					messages: response,
-					goingTo: "tools",
+					goingTo: "search_tool",
 				},
 			});
 		}
@@ -229,8 +203,6 @@ export class Supervisor {
 
 	private summarizerAgent = async (state: typeof AgentState.State) => {
 		const { summary, messages } = state;
-
-		console.log("summarizer agent called");
 
 		const responseSchema = z.object({
 			response: z
@@ -336,8 +308,6 @@ export class Supervisor {
 			name: "supervisor",
 		};
 
-		console.log("supervisor agent called", response);
-
 		return new Command({
 			goto: response.goto,
 			update: {
@@ -353,12 +323,17 @@ export class Supervisor {
 
 	public graph = new StateGraph(AgentState)
 		.addNode("supervisor", this.supervisorAgent)
-		.addNode("tools", tools, {
+		.addNode("weather_tool", weatherToolNode, {
 			retryPolicy: {
 				maxAttempts: 3,
 			},
 		})
 		.addNode("weather_expert", this.weatherAgent, {
+			retryPolicy: {
+				maxAttempts: 3,
+			},
+		})
+		.addNode("search_tool", searchToolNode, {
 			retryPolicy: {
 				maxAttempts: 3,
 			},
@@ -387,17 +362,15 @@ export class Supervisor {
 
 		.addConditionalEdges("weather_expert", this.hasToContinue, {
 			__end__: "__end__",
-			tools: "tools",
+			weather_tool: "weather_tool",
 		})
 
 		.addConditionalEdges("news_expert", this.hasToContinue, {
 			__end__: "__end__",
-			tools: "tools",
+			search_tool: "search_tool",
 		})
-		.addEdge("tools", "chat_agent")
-
-		.addEdge("weather_expert", "chat_agent")
-		.addEdge("news_expert", "chat_agent")
+		.addEdge("weather_tool", "chat_agent")
+		.addEdge("search_tool", "chat_agent")
 
 		.addConditionalEdges(
 			"chat_agent",
